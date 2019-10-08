0571f619807a22ce3693be95d02dfa59f7e47fd2
Jakub Barański <kubabar1@interia.pl>
2019-10-09 00:14:02 +0200

    Upgrade ScenesDataset and sample_batch

--- dataset/utils/Observation.py
+++ dataset/utils/Observation.py
@@ -1,14 +1,9 @@
 import torch
 
 class Observation:
-    def __init__(self, image, viewpoint):
-        self.check(image)
+    def __init__(self, image: torch.Tensor, viewpoint):
         self.image = image
         self.viewpoint = viewpoint
-
-    def check(self, image):
-        if not isinstance(image, torch.Tensor):
-            raise TypeError("Image needs to be a torch.Tensor")
 
     def to_array(self):
         return [self.image, self.viewpoint]
